b54631d2cd1455725188b94cf36806769ec50f08
simonfan <simonxd@gmail.com>
2014-01-15 04:51:43 -0200

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# argument-parser[![Build Status](https://secure.travis-ci.org/simonfan/argument-parser.png?branch=master)](http://travis-ci.org/simonfan/argument-parser)
+
+provide intelligent argument objects
---
+++ src/index.js
@@ -0,0 +1,30 @@
+//     ArgumentParser
+//     (c) simonfan
+//     ArgumentParser is licensed under the MIT terms.
+
+/**
+ * AMD and CJS module.
+ *
+ * @module ArgumentParser
+ */
+
+/* jshint ignore:start */
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+/* jshint ignore:end */
+
+define(function (require, exports, module) {
+	'use strict';
+
+	
+	var Lodash = require('lodash');
+	
+	var Subject = require('subject');
+	
+
+	var ArgumentParser = function ArgumentParser() {
+
+	};
+
+
+	return ArgumentParser;
+});
